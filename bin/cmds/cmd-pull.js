7ac0e5dadcef48d48671183604245cf0db8aca47
Pete Inge <pinge@bluecadet.com>
2023-10-19 11:54:10 -0400

    update documentation for pull

--- bin/cmds/cmd-pull.js
+++ bin/cmds/cmd-pull.js
@@ -2,10 +2,26 @@ let Logger = require('../../lib/utils/logger.js').getInstance();
 
 exports.command = 'pull'
 exports.desc = 'Pull down db and files assets from host.'
-exports.builder = (yargs) => {
 
+exports.builder = (yargs) => {
+  yargs
+    .boolean('init')
+    .describe('init', "Create config for DB connection")
+    .boolean('rebuild')
+    .describe('rebuild', "Delete existing config and create new file")
+    .boolean('force')
+    .alias('force', 'f')
+    .describe('force', "Force a new DB download")
+    .string('exclude')
+    .describe('exclude', "Allow custom excludes")
+
+    .string('file')
+    .describe('file', "To download a specific file in the public directory")
+    .string('dir')
+    .describe('dir', "To download a specific directory in the public directory")
   return yargs
 }
+
 exports.handler = function (args) {
   Logger.setVerb(args.v);
 
